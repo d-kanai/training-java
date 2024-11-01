fccf4bac63fb540cb86e0b16b42a54faf4eba549
d.kanai <contact@dkanai.com>
2024-11-01 11:16:08 +0900

    working

--- src/main/java/features/moneyFlow/application/MoneyChargeUsecase.java
+++ src/main/java/features/moneyFlow/application/MoneyChargeUsecase.java
@@ -3,6 +3,7 @@ package features.moneyFlow.application;
 import features.moneyFlow.domain.MoneyFlow;
 import features.moneyFlow.domain.MoneyFlowRepository;
 import features.moneyFlow.presentation.MoneyChargeInput;
+import shared.SqliteDatabase;
 
 import java.util.UUID;
 
@@ -15,6 +16,8 @@ public class MoneyChargeUsecase {
     }
 
     public void run(UUID userId, MoneyChargeInput input) {
+//        SqliteDatabase sqliteDatabase = new SqliteDatabase();
+//        sqliteDatabase.query("select * from articles;");
         MoneyFlow charge = MoneyFlow.charge(userId, input.value);
         moneyFlowRepository.save(charge);
     }
