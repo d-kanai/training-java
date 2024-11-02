63449ffdd11d3697ebdb8f04bec598917385532e
d.kanai <contact@dkanai.com>
2024-11-02 12:30:32 +0900

    working

--- src/main/java/features/order/application/OrderCreateUsecase.java
+++ src/main/java/features/order/application/OrderCreateUsecase.java
@@ -10,10 +10,23 @@ import features.product.domain.ProductRepository;
 import shared.Records;
 import shared.SqliteDatabase;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class OrderCreateUsecase {
 
     public void run(OrderCreateInput input) {
         Product product = new ProductRepository().findById(input.getProductId());
+
+        SqliteDatabase db = new SqliteDatabase();
+        Records records = db.find("select * from moneyFlows");
+        int sum = 0;
+        for (Object item : records.items) {
+            sum += (int)((Map)item).get("value");
+        }
+        if(sum< product.price()) {
+            throw new RuntimeException("お金が足りません");
+        }
 
         Order order = Order.create(product);
         new OrderRepository().save(order);
