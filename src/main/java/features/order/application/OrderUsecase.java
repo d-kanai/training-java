b93e3e9e2fc6a469901be8f9687705f663635c43
d.kanai <contact@dkanai.com>
2024-11-04 07:53:02 +0900

    working

--- src/main/java/features/order/application/OrderUsecase.java
+++ src/main/java/features/order/application/OrderUsecase.java
@@ -1,7 +1,13 @@
 package features.order.application;
 
-import features.order.domain.OrderService;
+import features.moneyFlows.domain.MoneyFlowRepository;
+import features.moneyFlows.domain.MoneyFlows;
+import features.order.domain.OrderFactory;
+import features.order.domain.OrderRepository;
+import features.order.domain.Ordered;
 import features.order.presentation.OrderCreateInput;
+import features.product.domain.ProductRepository;
+import features.product.domain.PublishedProduct;
 import features.user.domain.User;
 import features.user.domain.UserRepository;
 import shared.IMailSender;
@@ -11,16 +17,23 @@ import java.util.UUID;
 public class OrderUsecase {
 
     private final IMailSender mailSender;
-    UserRepository userRepository = new UserRepository();
-    OrderService orderService = new OrderService();
 
     public OrderUsecase(IMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
     public void run(UUID loginUserId, OrderCreateInput input) {
-        User user = userRepository.findById(loginUserId);
-        orderService.run(loginUserId, input.getProductId());
+        User user = new UserRepository().findById(loginUserId);
+        UUID productId = input.getProductId();
+
+        PublishedProduct product = new ProductRepository().findPublishedById(productId);
+        MoneyFlows moneyFlows = new MoneyFlowRepository().findByUserId(loginUserId);
+
+        Ordered ordered = new OrderFactory(loginUserId, moneyFlows, product).create();
+
+        new OrderRepository().save(ordered.order);
+        new MoneyFlowRepository().save(ordered.moneyFlow);
+
         if (user.plan() == User.Plan.VIP) {
             mailSender.send(user.email(), "for VIP");
         }
