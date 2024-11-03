2f737d2933a766b015789eb776aae10d5b58b3a4
d.kanai <contact@dkanai.com>
2024-11-04 06:53:27 +0900

    working

--- src/main/java/features/order/application/OrderCreateUsecase.java
+++ src/main/java/features/order/application/OrderCreateUsecase.java
@@ -1,24 +1,33 @@
 package features.order.application;
 
+import features.moneyFlows.domain.MoneyFlow;
 import features.moneyFlows.domain.MoneyFlowRepository;
-import features.moneyFlows.domain.MoneyFlows;
-import features.order.domain.OrderFactory;
+import features.order.domain.Order;
 import features.order.domain.OrderRepository;
-import features.order.domain.Ordered;
 import features.order.presentation.OrderCreateInput;
 import features.product.domain.Product;
 import features.product.domain.ProductRepository;
+
+import java.util.List;
 
 public class OrderCreateUsecase {
 
     public void run(OrderCreateInput input) {
         Product product = new ProductRepository().findById(input.getProductId());
-        MoneyFlows moneyFlows = new MoneyFlowRepository().findAll();
 
-        Ordered ordered = new OrderFactory(moneyFlows, product).create();
+        List<MoneyFlow> moneyFlows = new MoneyFlowRepository().findAll();
+        int sum = moneyFlows
+                .stream()
+                .mapToInt(MoneyFlow::value)
+                .sum();
 
-        new OrderRepository().save(ordered.order);
-        new MoneyFlowRepository().save(ordered.moneyFlow);
+        if (sum < product.price()) throw new RuntimeException("お金が足りません");
+
+        Order order = Order.create(product);
+        MoneyFlow moneyFlow = MoneyFlow.order(product);
+
+        new OrderRepository().save(order);
+        new MoneyFlowRepository().save(moneyFlow);
     }
 
 
