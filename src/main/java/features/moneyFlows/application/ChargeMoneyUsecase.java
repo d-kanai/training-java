8286dcf676693ead9d46125d3e293907323e916f
d.kanai <contact@dkanai.com>
2024-11-27 04:58:40 +0900

    working

--- src/main/java/features/moneyFlows/application/ChargeMoneyUsecase.java
+++ src/main/java/features/moneyFlows/application/ChargeMoneyUsecase.java
@@ -4,6 +4,12 @@ import features.moneyFlows.domain.MoneyFlow;
 import features.moneyFlows.domain.MoneyFlowRepository;
 import features.moneyFlows.presentation.ChargeMoneyInput;
 
+//・Static Factory
+//・No no name constructor
+//・completed constructor
+//・domain exception. domain validation. not usecase knowledge
+//・not transaction script => domain model
+
 public class ChargeMoneyUsecase {
     public void run(ChargeMoneyInput input) {
         int value = input.getValue();
--- src/main/java/features/order/application/OrderCreateUsecase.java
+++ src/main/java/features/order/application/OrderCreateUsecase.java
@@ -1,14 +1,8 @@
 package features.order.application;
 
-import features.moneyFlows.domain.MoneyFlow;
-import features.moneyFlows.domain.MoneyFlowRepository;
 import features.order.domain.Order;
 import features.order.domain.OrderRepository;
 import features.order.presentation.OrderCreateInput;
-import features.product.domain.Product;
-import features.product.domain.ProductRepository;
-import shared.Records;
-import shared.SqliteDatabase;
 
 public class OrderCreateUsecase {
 
