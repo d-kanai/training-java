bcc1114e00b496513e58495419f1ddba779ed10a
d.kanai <contact@dkanai.com>
2024-11-27 09:57:13 +0900

    working

--- src/main/java/features/product/domain/PublishedProduct.java
+++ src/main/java/features/product/domain/PublishedProduct.java
@@ -6,11 +6,11 @@ public class PublishedProduct extends Product {
 
     protected PublishedProduct(UUID id, String name, int price, Status status) {
         super(id, name, price, status);
+        if (status != Status.PUBLISHED) throw new RuntimeException("invalid state");
     }
 
     //NOTE: 永続データからの再構成用途
     public static PublishedProduct reconstruct(UUID id, String name, Integer price, Status status) {
-        if (status != Status.PUBLISHED) throw new RuntimeException("invalid state");
         return new PublishedProduct(id, name, price, status);
     }
 
