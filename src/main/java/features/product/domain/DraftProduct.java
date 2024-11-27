f5bc696c8103def3191e2d3d9ce6ecd78f8cfa83
d.kanai <contact@dkanai.com>
2024-11-27 09:58:14 +0900

    working

--- src/main/java/features/product/domain/DraftProduct.java
+++ src/main/java/features/product/domain/DraftProduct.java
@@ -6,11 +6,11 @@ public class DraftProduct extends Product {
 
     protected DraftProduct(UUID id, String name, int price, Status status) {
         super(id, name, price, status);
+        if (status == Status.PUBLISHED) throw new RuntimeException("すでに公開済みです");
     }
 
     //NOTE: 永続データからの再構成用途
     public static DraftProduct reconstruct(UUID id, String name, Integer price, Status status) {
-        if (status == Status.PUBLISHED) throw new RuntimeException("すでに公開済みです");
         return new DraftProduct(id, name, price, status);
     }
 
