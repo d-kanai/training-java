1300938870f6717d82b252d57ea745b25469ca5f
d.kanai <contact@dkanai.com>
2024-11-27 09:46:03 +0900

    working

--- src/main/java/features/product/application/ProductPublishUsecase.java
+++ src/main/java/features/product/application/ProductPublishUsecase.java
@@ -4,6 +4,11 @@ import features.product.domain.Product;
 import features.product.domain.ProductRepository;
 import features.product.presentation.ProductPublishInput;
 
+// ・status transition, nooooooooooo setter
+// ・manage "update"
+// ・1 class 1 public method => thin test. what about Logic Class?
+// ・save == upsert
+
 public class ProductPublishUsecase {
 
     ProductRepository productRepository = new ProductRepository();
