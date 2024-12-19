4390563e1d00b5e86aba98652c69cb4a0ef50f83
d.kanai <contact@dkanai.com>
2024-12-20 06:45:26 +0900

    working

--- src/main/java/features/user/application/UpgradeToVipUsecase.java
+++ src/main/java/features/user/application/UpgradeToVipUsecase.java
@@ -5,6 +5,7 @@ import features.user.domain.UserRepository;
 
 import java.util.UUID;
 
+//・model test ...? test strategy
 //・Design by Type MORE!!
 //・Each Status Class
 //・validation ? no, make Class. その振る舞いは誰ができるのか？
