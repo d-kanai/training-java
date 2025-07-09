6d80a53087f43377c93b3a0502da43d8b484870a
d-kanai <97098139+d-kanai@users.noreply.github.com>
2025-07-09 22:56:04 +0000

    working

--- src/test/java/e2e/steps/ProductSteps.java
+++ src/test/java/e2e/steps/ProductSteps.java
@@ -16,47 +16,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ProductSteps {
 
 
-    public static void ユーザが新規登録してログイン() {
-    }
+    public static void ユーザが新規登録してログイン() { }
 
-    public static void ユーザが商品をドラフトで登録() {
-    }
+    public static void ユーザが商品をドラフトで登録() { }
 
-    public static void ユーザが商品を公開() {
-    }
+    public static void ユーザが商品を公開() { }
 
-    public static void 商品が公開して登録されていること() {
-    }
+    public static void 商品が公開して登録されていること() { }
 
-    public static void ユーザのチャージ残高が5000円になっている() {
-    }
-
-    public static void ユーザが購入する() {
-        Records products = new SqliteDatabase().find("select * from products");
-        new OrderCreateUsecase().run(new OrderCreateInput(UUID.fromString((String) products.first().get("id"))));
-        Records orders = new SqliteDatabase().find("select * from orders");
-        assertEquals(1, orders.size()); // middle assertion
-        Records moneyFlows = new SqliteDatabase().find("select * from moneyFlows");
-        assertEquals(2, moneyFlows.size()); // middle assertion
+    public static void _5000円の商品が登録されている() {
+        new ProductCreateUsecase().run(new ProductCreateInput("book", 5000));
     }
 
     public static void ユーザが10000万円チャージする() {
         ChargeMoneyInput input = new ChargeMoneyInput(10000);
         new ChargeMoneyUsecase().run(input);
     }
-
-    public static void ユーザが商品を一覧を見る() {
+   
+   public static void ユーザが購入する() {
+        Records products = new SqliteDatabase().find("select * from products");
+        new OrderCreateUsecase().run(new OrderCreateInput(UUID.fromString((String) products.first().get("id"))));
+    
+        Records orders = new SqliteDatabase().find("select * from orders");
+        assertEquals(1, orders.size()); // middle assertion
+        Records moneyFlows = new SqliteDatabase().find("select * from moneyFlows");
+        assertEquals(2, moneyFlows.size()); // middle assertion
     }
 
-    public static void ユーザが商品を選択する() {
+    public static void ユーザのチャージ残高が5000円になっている() {
+        Records totalMoneyFlow = new SqliteDatabase().find("select sum(value) from moneyFlows");
+        assertEquals(5000, totalMoneyFlow.first().get("sum(value)"));
     }
 
-    public static void _5000円の商品が登録されている() {
-        new ProductCreateUsecase().run(new ProductCreateInput("book", 5000));
-    }
+    public static void ユーザが商品を一覧を見る() { }
+
+    public static void ユーザが商品を選択する() { }
 
     public static void 購入履歴一覧に商品が表示されている() {
-
     }
 
 }
